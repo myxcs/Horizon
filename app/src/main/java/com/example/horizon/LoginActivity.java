66cbfaedc6d7587ebfef14b5201f1ba395efa292
myxcs <118356147+myxcs@users.noreply.github.com>
2023-11-24 21:32:10 +0700

    wrong grammaly

--- app/src/main/java/com/example/horizon/LoginActivity.java
+++ app/src/main/java/com/example/horizon/LoginActivity.java
@@ -9,13 +9,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private Button loginBtn;
+    private TextView signupRedirectText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginBtn = findViewById(R.id.login_button);
+        signupRedirectText = findViewById(R.id.signup_re_button);
     }
 }
