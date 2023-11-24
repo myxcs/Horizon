f8c4cf217088d9dd0a60949cffef6f8c7f5ae527
myxcs <118356147+myxcs@users.noreply.github.com>
2023-11-24 21:22:25 +0700

    fisrt contact

---
+++ app/src/main/java/com/example/horizon/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.horizon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/horizon/SignupActivity.java
@@ -0,0 +1,80 @@
+package com.example.horizon;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignupActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private EditText signupEmail, signupPassword;
+    private Button signupBtn;
+    private TextView loginRedirectText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.signup_email);
+        signupPassword = findViewById(R.id.signup_password);
+        signupBtn = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.signup_text);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = signupEmail.getText().toString().trim();
+                String pass = signupPassword.getText().toString().trim();
+
+                if(user.isEmpty()){
+                    signupEmail.setError("Email không được để trống");
+                }
+                if(pass.isEmpty()){
+                    signupPassword.setError("Mật khẩu không được để trống");
+
+                }
+                if(pass.length() < 8){
+                    signupPassword.setError("Mật khẩu ít nhất có 8 kí tự");
+                }
+                else {
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(SignupActivity.this, "Đăng kí thành công", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                            }
+                            else {
+                                Toast.makeText(SignupActivity.this, "Đăng kí thất bại" +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+
+
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
