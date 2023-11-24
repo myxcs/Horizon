2672a94d04974c29f63f117ccced7a5b4a832549
myxcs <118356147+myxcs@users.noreply.github.com>
2023-11-25 01:39:46 +0700

    bottom_nav

--- app/src/main/java/com/example/horizon/MainActivity.java
+++ app/src/main/java/com/example/horizon/MainActivity.java
@@ -6,9 +6,38 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bo
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_menu);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.bottom_home:
+                    return true;
+                case R.id.bottom_search:
+                    startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    finish();
+                    return true;
+                case R.id.bottom_settings:
+                    startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    finish();
+                    return true;
+                case R.id.bottom_profile:
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    finish();
+                    return true;
+            }
+            return false;
+        });
     }
 }
