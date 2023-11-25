d00184ea4ec3c0df39585b84bcb4068850c41aa0
myxcs <118356147+myxcs@users.noreply.github.com>
2023-11-25 17:31:40 +0700

    bottom_nav run

--- app/src/main/java/com/example/horizon/Activity/MainActivity.java
+++ app/src/main/java/com/example/horizon/Activity/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.horizon.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.horizon.Fragment.HomeFragment;
 import com.example.horizon.Fragment.NotificationFragment;
@@ -11,6 +13,7 @@ import com.example.horizon.Fragment.ProfileFragment;
 import com.example.horizon.R;
 import com.example.horizon.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +29,25 @@ public class MainActivity extends AppCompatActivity {
 
     bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+    bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected( MenuItem item) {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                return true;
+            } else if (itemId == R.id.nav_notification) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, notificationFragment).commit();
+                return true;
+            } else if (itemId == R.id.nav_profile) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                return true;
+            }
+            return false;
+        }
+    });
 
-    }
 
-}
+
+}}
