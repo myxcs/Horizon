c0bd3e508106f61e00e9e402f926e075cee51a85
myxcs <118356147+myxcs@users.noreply.github.com>
2023-11-25 16:28:32 +0700

    bottom_nav

--- app/src/main/java/com/example/horizon/MainActivity.java
+++ app/src/main/java/com/example/horizon/MainActivity.java
@@ -2,42 +2,21 @@ package com.example.horizon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-
-
+     BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigationView = findViewById(R.id.bottom_nav);
 
-        Bo
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_menu);
-        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.bottom_home:
-                    return true;
-                case R.id.bottom_search:
-                    startActivity(new Intent(getApplicationContext(), SearchActivity.class));
-                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                    finish();
-                    return true;
-                case R.id.bottom_settings:
-                    startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
-                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                    finish();
-                    return true;
-                case R.id.bottom_profile:
-                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                    finish();
-                    return true;
-            }
-            return false;
-        });
+
     }
 }
