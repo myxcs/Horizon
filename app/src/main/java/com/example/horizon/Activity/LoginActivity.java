b23a9571a6079d2702f875fc22d46bb86e2d8527
myxcs <118356147+myxcs@users.noreply.github.com>
2023-11-25 22:43:51 +0700

    remember me 1

--- app/src/main/java/com/example/horizon/Activity/LoginActivity.java
+++ app/src/main/java/com/example/horizon/Activity/LoginActivity.java
@@ -3,11 +3,14 @@ package com.example.horizon.Activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.content.Intent;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +25,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
     private EditText loginEmail, loginPassword;
+
+    private CheckBox rememberMe;
     private Button loginBtn;
     private TextView signupRedirectText;
     @Override
@@ -32,8 +37,19 @@ public class LoginActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         loginEmail = findViewById(R.id.login_email);
         loginPassword = findViewById(R.id.login_password);
+        rememberMe = findViewById(R.id.checkbox);
         loginBtn = findViewById(R.id.login_button);
         signupRedirectText = findViewById(R.id.signup_re_button);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+        String checkbox = sharedPreferences.getString("remember","");
+        if(checkbox.equals("true")){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        else if(checkbox.equals("false")){
+            Toast.makeText(this, "vui lòng đăng nhập", Toast.LENGTH_SHORT).show();
+        }
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,7 +88,29 @@ public class LoginActivity extends AppCompatActivity {
 
         });
 
-                signupRedirectText.setOnClickListener(new View.OnClickListener() {
+
+
+       rememberMe.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+               if(compoundButton.isChecked()){
+                   SharedPreferences sharedPreferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                   SharedPreferences.Editor editor = sharedPreferences.edit();
+                   editor.putString("remember", "true");
+                   editor.apply();
+                   Toast.makeText(LoginActivity.this, "Checked", Toast.LENGTH_SHORT).show();
+               } else if (!compoundButton.isChecked()) {
+                   SharedPreferences sharedPreferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+                   SharedPreferences.Editor editor = sharedPreferences.edit();
+                   editor.putString("remember", "false");
+                   editor.apply();
+                   Toast.makeText(LoginActivity.this, "Unchecked", Toast.LENGTH_SHORT).show();
+               }
+           }
+       });
+
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         startActivity(new Intent(LoginActivity.this, SignupActivity.class));
