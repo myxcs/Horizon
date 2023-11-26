a52914c064376b412d7124edb43e5b9ac7f1a95f
myxcs <118356147+myxcs@users.noreply.github.com>
2023-11-27 02:53:57 +0700

    some update more

--- app/src/main/java/com/example/horizon/Activity/MainActivity.java
+++ app/src/main/java/com/example/horizon/Activity/MainActivity.java
@@ -2,17 +2,30 @@ package com.example.horizon.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.horizon.Adapters.GamesListAdapter;
 import com.example.horizon.Adapters.SliderAdapters;
+import com.example.horizon.Domian.ListGame;
 import com.example.horizon.Domian.SliderItems;
 import com.example.horizon.R;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager2 viewPager2;
     private Handler sliderHandler = new Handler();
+    private RecyclerView.Adapter adapterBestGames, AdapterUpcomingGames, adapterCategory;
+    private RecyclerView recyclerViewBestGames, recyclerViewUpcomingGames, recyclerViewCategory;
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest, mStringRequest2, mStringRequest3;
+    private ProgressBar loading1, loading2, loading3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,19 +73,39 @@ public class MainActivity extends AppCompatActivity {
 
 initView();
 banner();
+sendRequest();
 
 }
+
+    private void sendRequest() {
+        mRequestQueue = Volley.newRequestQueue(this);
+        loading1.setVisibility(View.VISIBLE);
+        mStringRequest = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies?page=1", response -> {
+            Gson gson = new Gson();
+            loading1.setVisibility(View.GONE);
+            ListGame items = gson.fromJson(response, ListGame.class);
+            adapterBestGames = new GamesListAdapter(items);
+            recyclerViewBestGames.setAdapter(adapterBestGames);
+        }, error -> {
+            loading1.setVisibility(View.GONE);
+            Log.i("myXcs", "onErrorResponse: " + error.toString());
+        });
+        mRequestQueue.add(mStringRequest);
+    }
 
     private void banner() {
         List<SliderItems> sliderItems = new ArrayList<>();
         sliderItems.add(new SliderItems(R.drawable.img_slider1));
         sliderItems.add(new SliderItems(R.drawable.img_slider2));
         sliderItems.add(new SliderItems(R.drawable.img_slider3));
+        sliderItems.add(new SliderItems(R.drawable.img_slider4));
+        sliderItems.add(new SliderItems(R.drawable.img_slider5));
 
         viewPager2.setAdapter(new SliderAdapters(sliderItems, viewPager2));
         viewPager2.setClipToPadding(false);
-        viewPager2.setOffscreenPageLimit(3);
-        viewPager2.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_ALWAYS);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(5);
+        viewPager2.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
 
         CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
         compositePageTransformer.addTransformer(new MarginPageTransformer(40));
@@ -110,6 +148,16 @@ banner();
     }
 
     private void initView() {
+
         viewPager2 = findViewById(R.id.viewpages);
+        recyclerViewBestGames = findViewById(R.id.view1);
+        recyclerViewBestGames.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewUpcomingGames = findViewById(R.id.view2);
+        recyclerViewUpcomingGames.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewCategory = findViewById(R.id.view3);
+        recyclerViewCategory.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        loading1 = findViewById(R.id.progressBar1);
+        loading2 = findViewById(R.id.progressBar2);
+        loading3 = findViewById(R.id.progressBar3);
     }
 }
