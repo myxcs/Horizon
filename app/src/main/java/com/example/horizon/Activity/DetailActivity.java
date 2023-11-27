6d42c7c039269746f7ac392898523cd16e131a01
myxcs <118356147+myxcs@users.noreply.github.com>
2023-11-28 04:41:46 +0700

    some more

--- app/src/main/java/com/example/horizon/Activity/DetailActivity.java
+++ app/src/main/java/com/example/horizon/Activity/DetailActivity.java
@@ -2,15 +2,29 @@ package com.example.horizon.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.horizon.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        back = findViewById(R.id.back_button);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
