40160905accd6796b0e84c680b93e0ee27731914
myxcs <118356147+myxcs@users.noreply.github.com>
2023-11-29 03:52:52 +0700

    login update

--- app/src/main/java/com/example/horizon/Activity/SignupActivity.java
+++ app/src/main/java/com/example/horizon/Activity/SignupActivity.java
@@ -5,31 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.horizon.Models.UserModel;
 import com.example.horizon.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private FirebaseAuth auth;
-    private EditText signupEmail, signupPassword;
-    private Button signupBtn;
-    private TextView loginRedirectText;
+    //tạo account
+
+     FirebaseAuth auth;
+     FirebaseDatabase database;
+     DatabaseReference reference;
+
+     UserModel userModel;
+     EditText signupEmail, signupPassword, signupName;
+     Button signupBtn;
+     TextView loginRedirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
+        //database cho phần đăng kí
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        signupName = findViewById(R.id.signup_name);
         signupEmail = findViewById(R.id.signup_email);
         signupPassword = findViewById(R.id.signup_password);
         signupBtn = findViewById(R.id.signup_button);
@@ -38,40 +56,13 @@ public class SignupActivity extends AppCompatActivity {
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user = signupEmail.getText().toString().trim();
-                String pass = signupPassword.getText().toString().trim();
 
-                if(user.isEmpty()){
-                    signupEmail.setError("Email không được để trống");
-                }
-                if(pass.isEmpty()){
-                    signupPassword.setError("Mật khẩu không được để trống");
-
-                }
-                if(pass.length() < 8){
-                    signupPassword.setError("Mật khẩu ít nhất có 8 kí tự");
-                }
-                else {
-                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()) {
-                                Toast.makeText(SignupActivity.this, "Đăng kí thành công", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
-                            }
-                            else {
-                                Toast.makeText(SignupActivity.this, "Đăng kí thất bại" +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
-
-
-
+                createUser();
             }
         });
 
         loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            //chuyển qua signin
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SignupActivity.this, LoginActivity.class));
@@ -79,4 +70,52 @@ public class SignupActivity extends AppCompatActivity {
         });
 
     }
+
+    private void createUser() {
+
+        //tạo user
+        String userMail = signupEmail.getText().toString();
+        String userPass = signupPassword.getText().toString();
+        String userName = signupName.getText().toString();
+
+        //ktra nhập
+        if (TextUtils.isEmpty(userName)) {
+            signupName.setError("Tên người dùng không được để trống");
+            return;
+        }
+        if(TextUtils.isEmpty(userMail)){
+            signupEmail.setError("Email không được để trống");
+            return;
+        }
+        if(TextUtils.isEmpty(userPass)){
+            signupPassword.setError("Mật khẩu không được để trống");
+            return;
+
+        }
+        if(userPass.length() < 8){
+            signupPassword.setError("Mật khẩu ít nhất có 8 kí tự");
+            return;
+        }
+        else {
+            auth.createUserWithEmailAndPassword(userMail, userPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                     //   set data user lên firebase
+                        UserModel userModel = new UserModel(userName, userMail, userPass);
+//                        String id = task.getResult().getUser().getUid();
+//                        database.getReference().child("Users").child(id).setValue(userModel);
+                        reference.push().setValue(userModel);
+                        Toast.makeText(SignupActivity.this, "Đăng kí thành công", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                    }
+                    else {
+                        Toast.makeText(SignupActivity.this, "Đăng kí thất bại: " +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
 }
